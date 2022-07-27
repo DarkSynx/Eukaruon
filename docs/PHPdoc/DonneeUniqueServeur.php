68285239de9bc1b96f78b3f02aafbaf9a3055b14
DarkSynx <dark.synx.github@gmail.com>
2022-07-27 13:08:20 +0200

    maj 1.06.04 MAJ PHPDOC

---
+++ docs/PHPdoc/DonneeUniqueServeur.php
@@ -0,0 +1,14 @@
+<?php class DonneeUniqueServeur {
+protected $test = '100000';
+protected $image = 'data/ok/lol';
+protected $listing = 'eko.dat';
+protected $linsting_var = array (
+  0 => 'test',
+  1 => 'image',
+  2 => 'listing',
+);
+public function get_linsting_var() { return = $linsting_var; }
+public function get_test() { return = $test; }
+public function get_image() { return = $image; }
+public function get_listing() { return = $listing; }
+}
---
+++ docs/PHPdoc/phpdoc.bat
@@ -0,0 +1,4 @@
+php phpDocumentor.phar -help
+pause
+php phpDocumentor.phar -i administration -i configs -i passerelle -i App -i Composer -i vendor -i docs -i bin -i .git -i .idea -i bdd -i config -i erreurs -i journaux -i page -i public -i page -i ressources -i src -i tests -i users -d C:\Users\synxcinaty\PhpstormProjects\Eukaruon -t C:\Users\synxcinaty\PhpstormProjects\Eukaruon\docs
+pause
